aa33445820f850f51d7ade90de73b2e8441115d9
denniskim-218 <dhkim218@gmail.com>
2026-01-19 23:36:09 +0900

    python_with_op_args,kwargs dags 업로드

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,21 @@
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from common.common_func import regist2
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    regist2_t1 = PythonOperator(
+        task_id='regist2_t1',
+        python_callable=regist2,
+        op_args=['dhkim','man','kr','seoul'],
+        op_kwargs={'email':'dhkim218@gmail.com','phone':'010-5882-8974'}
+    )
+
+    regist2_t1
+    
