e6fef6be645bfd19397a00b7393307a8a38596c7
denniskim-218 <dhkim218@gmail.com>
2026-01-19 20:57:04 +0900

    외부 공통 함수 추가

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,17 @@
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+import common.common_func import get_sftp
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
+    )
+
