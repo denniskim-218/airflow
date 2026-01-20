a7b85836324a97c5e839ce33d2b97543c880c8b5
denniskim-218 <dhkim218@gmail.com>
2026-01-20 15:38:43 +0900

    python_template dag 업로드

---
+++ dags/dags_python_template.py
@@ -0,0 +1,36 @@
+# 2025.01.20
+# Python Operator에서 Jinja템플릿 사용하기
+# typo 줄이기위해 직접 입력하면서 오타여부 확인
+
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_template",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    def python_function1(start_date, end_date, **kwargs):
+        print(start_date)
+        pring(end_date)
+
+    python_t1 = PythonOperator(
+        task_id='python_t1',
+        python_callable=python_function1,
+        op_kwargs={'start_date':'{{data_interval_start | ds}}', 'end_date':'{{data_interval_end | ds}}'}
+    )
+
+    @task(task_id='python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ds:' + kwargs['ts'])
+        print('data_interval_start:' + str(kwargs['data_interval_start']))
+        print('data_interval_end:' + str(kwargs['data_interval_end']))
+        print('task_instance:' + str(kwargs['ti']))
+
+    python_t1 >> python_function2()
