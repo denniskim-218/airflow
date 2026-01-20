928f324110ca4bcd77821bae046b58627a04b295
denniskim-218 <dhkim218@gmail.com>
2026-01-20 18:33:32 +0900

    bash_with_macr예제 업로드

---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,23 @@
+# 2026.01.20 - bash_with_macro_eg2
+# from, import 선언부 순서 상관없음
+
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg2",
+    schedule="10 0 * * 6#2",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
+    bash_task_2 = BashOperator(
+        task_id='bash_task_2',
+        env={'START_DATE':'{{(data_inverval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
+             'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && "END_DATE: $END_DATE"'
+    )
+    
