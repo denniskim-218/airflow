02948c5461ace40521281670bbd7e6d92a5b7dad
denniskim-218 <dhkim218@gmail.com>
2026-01-20 19:08:10 +0900

    bash_with_macro dags 오타실습인가ㅠ

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -1,6 +1,7 @@
 # 2026.01.20 - bash_with_macro_eg1
 # from, import 선언부 순서 상관없음
 # env={'START_DATE':'{{data_inverval_start~~ -> env={'START_DATE':'{{data_interval_start~~
+# macro.dateutil.~ -> macros.dateutil.~
 
 
 from airflow.sdk import DAG
@@ -18,7 +19,7 @@ with DAG(
     bash_task_1 = BashOperator(
         task_id='bash_task_1',
         env={'START_DATE':'{{data_interval_start.in_timezone("Asia/Seoul") | ds}}',
-             'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
+             'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
         },
         bash_command='echo "START_DATE: $START_DATE" && "END_DATE: $END_DATE"'
     )
