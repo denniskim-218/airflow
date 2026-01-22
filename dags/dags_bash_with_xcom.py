0a928c0e5d5e22dcea9788e73aef2e46abd2b5f6
denniskim-218 <dhkim218@gmail.com>
2026-01-22 14:10:09 +0900

    upload dags_bash_with_xcom.py

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,34 @@
+# 2026.01.22 - bash_with_xcom
+# from, import 선언부 순서 상관없음
+
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_xcom",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # bash_command 이용한 task
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_command="echo start &&"
+                     "echo XCOM_PUSHED"
+                     "{{ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
+                     "echo COMPLETE"
+    )
+    # env 활용한 task
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        #env={'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed')}}",
+        env={'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed', task_ids='bash_push')}}",
+            'RETURN_VALUE':"{{ti.xcom_pull(task_ids='bash_push')}}"},
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
+        do_xcom_push=False
+    )
+
+    bash_push >> bash_pull
+    
