880dbc230e5be60a796f83a9ebf7fd39b8dfe9bb
denniskim-218 <dhkim218@gmail.com>
2026-01-26 11:04:55 +0900

    실습 dag 업로드

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -1,22 +1,25 @@
-# 2026.01.26 - python_with_branch_decorator
+# 2026.01.26 - base_branch_operator
 # from, import 선언부 순서 상관없음
-# datatime만 import하면 오류남
 
-from airflow.sdk import DAG, task
-import datetime
+from airflow.sdk import DAG
 import pendulum
+import datetime
+from airflow.providers.standard.operators.branch import BaseBranchOperator
 from airflow.providers.standard.operators.python import PythonOperator
 
 with DAG(
-    dag_id="dags_python_with_branch_decorator",
+    dag_id="dags_base_branch_operator",
     schedule=None,
     start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    # task branch 선언
-    @task.branch(task_id='python_branch_task')
-    def select_random():
+
+    # 클래스 상속을 위한 선언
+    class CustomerBranchOperator(BaseBranchOperator)
+    def choose_barch(self, context):
         import random
+        print(context)
+
         item_lst = ['A','B','C']
         selected_item = random.choice(item_lst)
         if selected_item == 'A':
@@ -24,6 +27,8 @@ with DAG(
         elif selected_item in ['B','C']:
             return ['task_b','task_c']
     
+    custom_branch_operator = CustomerBranchOperator(task_id='python_barch_task')
+
     def common_func(**kwargs):
         print(kwargs['selected'])
 
@@ -45,5 +50,5 @@ with DAG(
         op_kwargs={'selected':'C'}
     )
 
-    select_random() >> [task_a, task_b, task_c]
+    custom_branch_operator >> [task_a, task_b, task_c]
     
