0b29be7a06fc26dfaaf8f525eef563d3a8fb6f71
denniskim-218 <dhkim218@gmail.com>
2026-01-26 10:51:24 +0900

    reupload dags_python_with_branch_decorator

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -3,13 +3,13 @@
 
 from airflow.sdk import DAG, task
 import datetime
-# import pendulum
+import pendulum
 from airflow.providers.standard.operators.python import PythonOperator
 
 with DAG(
     dag_id="dags_python_with_branch_decorator",
     schedule=None,
-    start_date=datetime(2026, 1, 1),
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     # task branch 선언
